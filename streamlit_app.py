4fc5b014fa6cc89b794914a3107b409bc1d35ea1
ChantelleLum <chantellelum2212@gmail.com>
2024-10-23 14:28:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,16 +13,9 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-# option = st.selectbox(
-#     "What is your favourite fruit?",
-#     ("Banana", "Strawberry", "Peaches"),
-# )
-
-# st.write("Your favourite fruit is:", option)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: '
@@ -31,23 +24,15 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
 
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    # st.write(ingredients_string)
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
-    # st.write(my_insert_stmt)
-    # st.stop    
     
-    # st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
